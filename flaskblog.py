b3a0bcb0f29df0e50c26a76511af62d3d0cfc06d
Kiseki <42840796+kisenakamoto@users.noreply.github.com>
2023-01-01 21:55:59 +0800

    Lesson 1-3
    Templates, Forms and User Input

---
+++ flaskblog.py
@@ -0,0 +1,56 @@
+# save this as app.py
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+import requestlib
+
+app = Flask(__name__)
+
+#print(requestlib.updateTime())
+app.config['SECRET_KEY'] = '60a5afb5362c8056f7c3d755584be6f7'
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login(): #url_for('login')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- requestlib.py
+++ requestlib.py
@@ -1,9 +1,15 @@
 import requests
+import json
+
 
 r = requests.get('https://www.solaxcloud.com/proxyApp/proxy/api/getRealtimeInfo.do?tokenId=20221026130839175716859&sn=SVT7CYCWCP')
 
 library = r.json()
 result = library.get('result')
-# result = library['result']
-print(library) 
-print('\n', result.get('uploadTime'))
+
+def updateTime():
+	return result.get('uploadTime')
+	# print(json.dumps(library, indent=2)) 
+	# print(result.get('uploadTime'))
+	# print(type(library)) 
+
