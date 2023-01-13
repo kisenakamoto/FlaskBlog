5a180a0fe3e409daeb06c07a953cdd5dcfd8d0b4
Kiseki <42840796+kisenakamoto@users.noreply.github.com>
2023-01-13 19:52:22 +0800

    11 - Blueprints and Configuration

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from flaskblog import app
+from flaskblog import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-#     from flaskblog import app, db
-# >>> app.app_context().push()
-# >>> db.create_all()
